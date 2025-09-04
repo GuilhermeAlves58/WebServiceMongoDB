42cd45e79e69eb50ceac5f198da54c916c43aff4
perkles <guilhermealves5194@gmail.com>
2025-09-04 17:31:22 -0300

    Project Created

---
+++ myfirstmongodb/src/main/java/com/webservicemongodb/myfirstmongodb/MyfirstmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.webservicemongodb.myfirstmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstmongodbApplication.class, args);
+	}
+
+}
---
+++ myfirstmongodb/src/test/java/com/webservicemongodb/myfirstmongodb/MyfirstmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webservicemongodb.myfirstmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
