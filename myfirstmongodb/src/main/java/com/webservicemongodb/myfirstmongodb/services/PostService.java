4209b9d6c90c65f449c559bb21b4c9d0eb3c3c02
perkles <guilhermealves5194@gmail.com>
2025-09-06 23:11:56 -0300

    Obtendo um post por id

---
+++ myfirstmongodb/src/main/java/com/webservicemongodb/myfirstmongodb/services/PostService.java
@@ -0,0 +1,23 @@
+package com.webservicemongodb.myfirstmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.webservicemongodb.myfirstmongodb.domain.Post;
+import com.webservicemongodb.myfirstmongodb.repository.PostRepository;
+import com.webservicemongodb.myfirstmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
