8a36c9e3364ebfe9ac95e23dde2fa2f368182bfc
perkles <guilhermealves5194@gmail.com>
2025-09-07 16:59:55 -0300

    Acrescentando comentários aos posts

--- myfirstmongodb/src/main/java/com/webservicemongodb/myfirstmongodb/config/Instantiation.java
+++ myfirstmongodb/src/main/java/com/webservicemongodb/myfirstmongodb/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.webservicemongodb.myfirstmongodb.domain.Post;
 import com.webservicemongodb.myfirstmongodb.domain.User;
 import com.webservicemongodb.myfirstmongodb.dto.AuthorDTO;
+import com.webservicemongodb.myfirstmongodb.dto.CommentDTO;
 import com.webservicemongodb.myfirstmongodb.repository.PostRepository;
 import com.webservicemongodb.myfirstmongodb.repository.UserRepository;
 
@@ -38,7 +39,14 @@ public class Instantiation implements CommandLineRunner {
 
         Post post0 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo.Abraços!",new AuthorDTO(maria));
         Post post1 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia", sdf.parse("23/03/2018"), new AuthorDTO(alex));
         
+        post0.getComments().addAll(Arrays.asList(c1,c2));
+        post1.getComments().addAll(Arrays.asList(c3));
+
         userRepository.saveAll(Arrays.asList(maria,alex,bob));
         postRepository.saveAll(Arrays.asList(post0,post1));
 
