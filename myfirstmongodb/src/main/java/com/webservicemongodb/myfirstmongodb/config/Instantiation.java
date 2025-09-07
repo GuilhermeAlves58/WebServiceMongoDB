7e3ae6ce98ac7c1f9f911f35415d8d2925312625
perkles <guilhermealves5194@gmail.com>
2025-09-06 23:05:05 -0300

    Endpoint para retornar os posts de um usuário

--- myfirstmongodb/src/main/java/com/webservicemongodb/myfirstmongodb/config/Instantiation.java
+++ myfirstmongodb/src/main/java/com/webservicemongodb/myfirstmongodb/config/Instantiation.java
@@ -37,7 +37,7 @@ public class Instantiation implements CommandLineRunner {
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
         Post post0 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo.Abraços!",new AuthorDTO(maria));
-        Post post1 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje", new AuthorDTO(alex));
+        Post post1 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje", new AuthorDTO(maria));
         
         userRepository.saveAll(Arrays.asList(maria,alex,bob));
         postRepository.saveAll(Arrays.asList(post0,post1));
--- myfirstmongodb/src/main/java/com/webservicemongodb/myfirstmongodb/resources/UserResource.java
+++ myfirstmongodb/src/main/java/com/webservicemongodb/myfirstmongodb/resources/UserResource.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.webservicemongodb.myfirstmongodb.domain.Post;
 import com.webservicemongodb.myfirstmongodb.domain.User;
 import com.webservicemongodb.myfirstmongodb.dto.UserDTO;
 import com.webservicemongodb.myfirstmongodb.services.UserService;
@@ -61,5 +62,10 @@ public class UserResource {
         obj = service.update(obj);
 
         return ResponseEntity.noContent().build();
+    }
+    @RequestMapping(value = "/{id}/posts",method=RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
